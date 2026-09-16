e6e70447fd7a38ad90130cade49e43ee5cde4faf
Dahtmusa <musahassan575+1@gmail.com>
2026-09-16 16:13:55 +0100

    Mount CMS-driven homepage testimonials

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
 import PublicHomepage from '@/components/PublicHomepageSchool';
+import HomeTestimonialsPortal from '@/components/HomeTestimonialsPortal';
 
 export default function HomePage() {
-  return <PublicHomepage />;
+  return (
+    <>
+      <PublicHomepage />
+      <HomeTestimonialsPortal />
+    </>
+  );
 }
