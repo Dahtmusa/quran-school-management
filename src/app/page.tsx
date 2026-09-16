c802ba65cc806e62aec6e3b31abe46c86f5f3d80
Dahtmusa <musahassan575+1@gmail.com>
2026-09-16 16:15:33 +0100

    Restore original leadership and teaching staff presentation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import PublicHomepage from '@/components/PublicHomepageSchool';
 import HomeTestimonialsPortal from '@/components/HomeTestimonialsPortal';
+import RestoredPeoplePortal from '@/components/RestoredPeoplePortal';
 
 export default function HomePage() {
   return (
     <>
       <PublicHomepage />
+      <RestoredPeoplePortal />
       <HomeTestimonialsPortal />
     </>
   );
