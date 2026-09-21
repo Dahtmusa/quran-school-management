4929cfe49c8bcbe0e21211af171dbfc6c66cb5d1
Dahtmusa <musahassan575+1@gmail.com>
2026-09-21 16:33:06 +0100

    Keep homepage composed as one cohesive public experience

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,5 @@
 import PublicHomepage from '@/components/PublicHomepageSchool';
-import HomeTestimonialsPortal from '@/components/HomeTestimonialsPortal';
-import RestoredPeoplePortal from '@/components/RestoredPeoplePortal';
 
 export default function HomePage() {
-  return (
-    <>
-      <PublicHomepage />
-      <RestoredPeoplePortal />
-      <HomeTestimonialsPortal />
-    </>
-  );
+  return <PublicHomepage />;
 }
